5b91634adbed23a884b6bdb1763e907d42e9db4a
Evans PARFAIT <fani.prft@gmail.com>
2025-03-22 15:20:52 +0100

    Home

---
+++ frontend/src/pages/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import './profile.css';
+import { motion } from 'framer-motion';
+
+const Profile = ({ match }) => {
+    const { id } = useParams();
+    return (
+        <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -20, opacity: 0 }}
+            transition={{ duration: 1, ease: "easeInOut" }}
+        >
+            <div className="profile">
+                <h1>Profil {id}</h1>
+            </div>
+        </motion.div>
+    )
+}
+
+export default Profile;
