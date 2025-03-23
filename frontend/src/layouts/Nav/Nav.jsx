11afab477b612622356153aef8e17a62723c1676
Evans PARFAIT <fani.prft@gmail.com>
2025-03-24 00:07:13 +0100

    Style de formulaire et modification de la nav en fonction de l'utilisateur connecté

--- frontend/src/layouts/Nav/Nav.jsx
+++ frontend/src/layouts/Nav/Nav.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import './nav.css';
 import Button from "../../components/Button/Button";
 import { IoMdNotificationsOutline } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
 
-const Nav = () => {
+const Nav = ({ user, setUser }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleLogout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
+
+
     return (
         <nav className="nav">
             <div className="nav__list">
@@ -14,12 +24,12 @@ const Nav = () => {
                 <div className="nav__line"></div>
                 <div className="nav__links">
                     <Link to="/" className="link">Accueil</Link>
-                    <Link to="/categories" className="link">Categories</Link>
-                    {/* <Link to="/profile/:id" className="link">Profil</Link> */}
-                    <Link to="/pricing" className="link">Pricing</Link>
-                    <Link to="/about" className="link">About</Link>
+                    <Link to="/categories" className="link">Catégories</Link>
+                    <Link to="/pricing" className="link">Tarifs</Link>
+                    <Link to="/about" className="link">À propos</Link>
                 </div>
             </div>
+
             <div className="nav__buttons">
                 <Button
                     type="icon"
@@ -29,27 +39,58 @@ const Nav = () => {
                     icon={IoMdNotificationsOutline}
                 />
 
-                <Button
-                    type="text"
-                    backgroundColor="transparent"
-                    color="#fff"
-                    border="1px solid #fff"
-                >
-                    Se connecter
-                </Button>
+                {user ? (
+                    <div className="nav__profile">
+                        <Button
+                            className="profile-icon"
+                            type="icon"
+                            backgroundColor="#fff"
+                            color="hsl(240, 3%, 8%)"
+                            border="1px solid #fff"
+                            onClick={() => setMenuOpen(!menuOpen)}
+                            icon={FaRegUser}
+                        >
+                        </Button>
+                        {menuOpen && (
+                            <div className="dropdown-menu">
+                                <div className="profile__img">
+                                    <span>{user.firstname[0]}</span>
+                                </div>
+                                <p className="username">{user.firstname} {user.lastname}</p>
+                                <p className="email">{user.email}</p>
+                                <Link to="/profile" className="button-profil">Mon Profil</Link>
+                                <button onClick={handleLogout} className="button-logout">Déconnexion</button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        <Link to="/login" style={{ color: "#fff", textDecoration: "none" }}>
+                            <Button
+                                type="text"
+                                backgroundColor="transparent"
+                                color="#fff"
+                                border="1px solid #fff"
+                            >
+                                Se connecter
+                            </Button>
+                        </Link>
 
-                <Button
-                    type="text"
-                    backgroundColor="#fff"
-                    color="hsl(240, 3%, 8%)"
-                    border="1px solid #fff"
-                >
-                    S'inscrire
-                </Button>
-
+                        <Link to="/register" style={{ color: "#000", textDecoration: "none" }}>
+                            <Button
+                                type="text"
+                                backgroundColor="#fff"
+                                color="hsl(240, 3%, 8%)"
+                                border="1px solid #fff"
+                            >
+                                S'inscrire
+                            </Button>
+                        </Link>
+                    </>
+                )}
             </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Nav;
